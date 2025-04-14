634194a54e5a44bc2b704ee6df5e7e2808976802
Janis Jansen <oss@janis.me>
2025-04-14 15:49:21 +0200

    somewhat improve the playground (allow setting the example via query param)

--- packages/playground/src/App.tsx
+++ packages/playground/src/App.tsx
@@ -1,89 +1,42 @@
 import { Terminal as XTerm } from '@xterm/xterm';
-import c from 'ansi-colors';
+import { useAtom } from 'jotai';
 import { Resizable } from 're-resizable';
 import { useEffect, useRef, useState } from 'react';
-import * as sass from 'sass';
 
+import { editorAtom, exampleAtom } from './atoms';
+import { compile } from './compile';
 import Editor from './components/Editor/Editor';
 import Header from './components/Header/Header';
 import Terminal from './components/Terminal/Terminal';
-import { useDebounce } from './hooks/useDebounce';
+import { EXAMPLES } from './constants';
 
 import './App.scss';
-
-import { useAtom } from 'jotai';
-
-import { editorAtom } from './atoms';
-
-const themed = {
-  '': await import('@janis.me/themed?raw'),
-  '/modifiers': await import('@janis.me/themed/modifiers?raw'),
-  '/modifiers/alpha': await import('@janis.me/themed/modifiers/alpha?raw'),
-  '/modifiers/colorspace': await import('@janis.me/themed/modifiers/colorspace?raw'),
-  '/modifiers/fill': await import('@janis.me/themed/modifiers/fill?raw'),
-  '/modifiers/lightness': await import('@janis.me/themed/modifiers/lightness?raw'),
-  '/modifiers/saturation': await import('@janis.me/themed/modifiers/saturation?raw'),
-};
 
 function App() {
   const xTermRef = useRef<XTerm>(null);
 
   const [editorValue, setEditorValue] = useAtom(editorAtom);
+  const [example, setExample] = useAtom(exampleAtom);
   const [resultValue, setResultValue] = useState<string>('');
-  const debouncedEditorValue = useDebounce(editorValue, 200);
-
-  const compile = async (val: string) => {
-    if (val == '' || !xTermRef.current) return;
-    xTermRef.current.writeln('Compiling...');
-
-    try {
-      const res = sass.compileString(val, {
-        alertAscii: true,
-        alertColor: true,
-        importers: [
-          {
-            canonicalize(url) {
-              const sanitized = url.replace('pkg:', '');
-              if (!sanitized.startsWith('@janis.me/themed'))
-                throw new Error('@use statements only work with @janis.me/themed');
-
-              return new URL(`pkg:${sanitized}`);
-            },
-            load(canonicalUrl) {
-              const sanitized = canonicalUrl.href.replace('pkg:', '').replace('.scss', '');
-              const path = sanitized.split('@janis.me/themed')[1] as keyof typeof themed;
-              const contents = themed[path]?.default;
-
-              if (!contents) throw new Error(`Could not resolve module ${canonicalUrl}`);
-
-              return {
-                contents,
-                syntax: 'scss',
-              };
-            },
-          },
-        ],
-      });
-      setResultValue(res.css);
-      xTermRef.current.clear();
-      xTermRef.current.writeln('Compiled successfully!');
-    } catch (error) {
-      xTermRef.current.clear();
-      xTermRef.current.writeln('Error:');
-      xTermRef.current.writeln((error as Error).message);
-      setResultValue('');
-    }
-  };
 
   useEffect(() => {
-    if (debouncedEditorValue) {
-      compile(debouncedEditorValue);
+    if (editorValue && xTermRef.current) {
+      const res = compile(editorValue, xTermRef.current);
+      setResultValue(res);
     } else {
       setResultValue('');
     }
-  }, [debouncedEditorValue]);
+  }, [editorValue]);
+
+  useEffect(() => {
+    if (example) {
+      setEditorValue(EXAMPLES[example]);
+    }
+  }, [example, setEditorValue]);
 
   const handleEditorChange = (value: string | undefined) => {
+    setExample(null);
+
     if (value) {
       setEditorValue(value);
     }
@@ -91,8 +44,10 @@ function App() {
 
   const handleTerminalMount = (xterm: XTerm) => {
     xTermRef.current = xterm;
-    // re-compile after editor is done
-    compile(debouncedEditorValue);
+    // force recompilation after xterm is mounted
+    if (editorValue) {
+      setResultValue(compile(editorValue, xterm));
+    }
   };
 
   const handleTerminalResize = (_: { rows: number; cols: number }) => {};
--- packages/playground/src/atoms.ts
+++ packages/playground/src/atoms.ts
@@ -1,5 +1,11 @@
 import { atom } from 'jotai';
 
-import { EXAMPLE_SIMPLE } from './constants';
+import { EXAMPLES } from './constants';
 
-export const editorAtom = atom(EXAMPLE_SIMPLE);
+const urlParams = new URLSearchParams(window.location.search);
+const exampleParam = urlParams.get('example') as keyof typeof EXAMPLES;
+const initialExample = Object.keys(EXAMPLES).includes(exampleParam) ? exampleParam : 'simple';
+const initialCode = EXAMPLES[initialExample];
+
+export const exampleAtom = atom<typeof initialExample | null>(initialExample);
+export const editorAtom = atom<string>(initialCode);
