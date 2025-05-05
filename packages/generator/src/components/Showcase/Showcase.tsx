a1fbaa4fa70d1a32324b184a019722c04987f6da
Janis Jansen <oss@janis.me>
2025-05-05 15:58:03 +0200

    feat: Start adding `@janis.me/ui` for showcase, add `1` as a size step to generator

--- packages/generator/src/components/Showcase/Showcase.tsx
+++ packages/generator/src/components/Showcase/Showcase.tsx
@@ -1,11 +1,12 @@
 import { CheckIcon } from '@radix-ui/react-icons';
 
-import { Checkbox } from '@janis.me/ui';
+import { Checkbox, DropdownMenu, Label } from '@janis.me/ui';
 
 export default function Showcase() {
   return (
     <div className="showcase">
-      <Checkbox.Root>
+      <Label.Root htmlFor="checkbox1">Checkbox</Label.Root>
+      <Checkbox.Root id="checkbox1">
         <Checkbox.Indicator>
           <CheckIcon />
         </Checkbox.Indicator>
